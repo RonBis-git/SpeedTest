14278efa31d1e4661dcfca5fba5694b7cf1f0d32
RonBis-git <rohanbiswas1993@gmail.com>
2024-01-29 15:52:05 +0000

    Rewritten it to call numpy and eigen functions separately.

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import numpy as np
 
+import sys
 
-def np_operation(rows):
-    mat = np.random.default_rng().uniform(0, 1, size=(rows, 3))
-    d_mat = np.arange(9).reshape(3, 3)
-    print(d_mat)
-    d0 = d_mat[2,:]
-    print(d0)
+sys.path.append("build")
+import cppOps
+import npOps
+
+def operations(rows):
+    return np.random.default_rng().uniform(0, 1, size=(rows, 3))
 
 
 if __name__ == "__main__":
-    np_operation(2)
+    s_mat = np.arange(9).reshape(3, 3)
+    print(s_mat)
+    f_mat = np.arange(100,109).reshape(3,3)
+    print(f_mat)
+    axis = np.random.default_rng().uniform(0,3,size=3)
+    angle = np.pi/4
+    vecs = operations(rows=2)
+
+    print('Numpy results')
+    np_rotvecs = npOps.rotate_vectors(s_mat, f_mat, axis, angle, vecs)
+    print(np_rotvecs)
+
+    print('Eigen results')
+    eig_rotvecs = cppOps.get_rotated_vectors(s_mat, f_mat, axis, angle, vecs)
+    print(eig_rotvecs)
+
