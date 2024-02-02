f9347015fab9e2edae33ebfa66e684596e44ba00
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-02 12:37:16 +0000

    Refactor: Included eigen.h header from pybind11, renamed package and function name.

--- bindpy.cpp
+++ bindpy.cpp
@@ -2,13 +2,13 @@
 #include "eigOps.h"
 
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
 #include <pybind11/numpy.h>
 
 namespace py = pybind11;
 
 
-PYBIND11_MODULE(eigOp, m)
+PYBIND11_MODULE(cppOps, m)
 {
-    m.def("get_rotated_vector", &get_rotated_vector, py::return_value_policy::reference_internal);
+    m.def("eig_get_rotated_vector", &eig_get_rotated_vector, py::return_value_policy::reference_internal);
 }
