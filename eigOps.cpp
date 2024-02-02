2cae84ccca68c3cea8e68e8232166c1e361ae8a4
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-02 12:29:01 +0000

    Refactor: Optimised to make the code faster.
    The following changes were made after many tests.
    	The return type is changed to Eigen::Matrix3Xd to return a set of rotated vectors as column vectors. These will be transposed in the python file.
    	The vectors arguement in the function has been changed to Eigen::Matrix3Xd. One should be carefull to call this function as a 3xN array rather than Nx3 array. Hence, transposing the arguement before calling is recommended.
    Note: Transposing a large matrix eg. 100000 x 3 size slows down the code by almost 3x.

--- eigOps.cpp
+++ eigOps.cpp
@@ -2,18 +2,28 @@
 
 #include "eigOps.h"
 
-Eigen::MatrixXd get_rotated_vector(const Eigen::Ref<const Eigen::Matrix3d> &set_mat,
-                                   const Eigen::Ref<const Eigen::Matrix3d> &fixed_mat,
-                                   const Eigen::Ref<const Eigen::Vector3d> &axis,
-                                   const double angle,
-                                   const Eigen::Ref<const Eigen::MatrixXd> &vectors)
+Eigen::Matrix3Xd eig_get_rotated_vector(const Eigen::Ref<const Eigen::Matrix3d> &set_mat,
+                               const Eigen::Ref<const Eigen::Matrix3d> &fixed_mat,
+                               const Eigen::Ref<const Eigen::Vector3d> &axis,
+                               const double angle,
+                               const Eigen::Ref<const Eigen::Matrix3Xd> &vectors)
 {
-    Eigen::Matrix3d rot_mat = set_mat * Eigen::AngleAxisd(angle, axis).matrix() * fixed_mat;
-    std::cout << 'Setting Matrix = ' << set_mat << std::endl;
-    std::cout << 'Fixed Matrix = ' << fixed_mat << std::endl;
-    std::cout << 'Axis = ' << axis << std::endl;
-    std::cout << 'Angle = ' << angle << std::endl;
-    std::cout << 'Vectors = ' << vectors << std::endl;
+    Eigen::Matrix3d rot_mat;
+    rot_mat =  set_mat* Eigen::AngleAxisd(angle, axis.normalized()).toRotationMatrix() * fixed_mat;
+    // Eigen::Affine3d q;
+    // rot_mat.translation() << 0.0, 0.0, 0.0;
+    // q.linear() = set_mat* Eigen::AngleAxisd(angle, axis.normalized()).toRotationMatrix() * fixed_mat;
+    // std::cout << "Setting Matrix = " << set_mat << std::endl;
+    // std::cout << "Fixed Matrix = " << fixed_mat << std::endl;
+    // std::cout << "Axis = " << axis.normalized() << std::endl;
+    // std::cout << "Angle = " << angle << std::endl;
+    // // std::cout << "Vectors = " << vectors << std::endl;
+    // std::cout << "Rows = " << vectors.rows() << "Columns = " << vectors.cols() << std::endl;
+    // std::cout << "Rotation Matrix = " << q.linear() << std::endl;
     return rot_mat * vectors;
 }
 
+void print_passed(const Eigen::Ref<const Eigen::RowVector3d> &mat)
+{
+    std::cout << mat << std::endl;
+}
