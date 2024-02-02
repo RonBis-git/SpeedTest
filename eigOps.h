6cbd5fb1e3e7ccd6b8366c61cc756b9b3066d995
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-02 12:34:06 +0000

    Refactor: added header guards and changed function return type and arguement type of last arguement.

--- eigOps.h
+++ eigOps.h
@@ -1,10 +1,14 @@
+#ifndef EIGOPS_H
+#define EIGOPS_H
+#endif
+
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-
-Eigen::MatrixXd get_rotated_vector(const Eigen::Ref<const Eigen::Matrix3d> &,
-                                   const Eigen::Ref<const Eigen::Matrix3d> &,
-                                   const Eigen::Ref<const Eigen::Vector3d> &,
-                                   const double,
-                                   const Eigen::Ref<const Eigen::MatrixXd> &);
+Eigen::Matrix3Xd eig_get_rotated_vector(const Eigen::Ref<const Eigen::Matrix3d> &,
+                               const Eigen::Ref<const Eigen::Matrix3d> &,
+                               const Eigen::Ref<const Eigen::Vector3d> &,
+                               const double,
+                               const Eigen::Ref<const Eigen::Matrix3Xd> &);
+void print_passed(const Eigen::Ref<const Eigen::RowVector3d> &);
