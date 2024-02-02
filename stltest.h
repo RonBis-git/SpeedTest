80b9eca702f617ce961c1bca9cacc06545a1277f
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-02 14:38:37 +0000

    Included function definitions and datatype definitions.

---
+++ stltest.h
@@ -0,0 +1,25 @@
+#ifndef STLTEST_H 
+#define STLTEST_H
+
+
+#include <vector>
+
+#endif
+
+using vec2D = std::vector<std::vector<double>>;
+using vec1D = std::vector<double>;
+
+double get_norm(const vec1D &);
+vec1D normalize(const vec1D &);
+vec2D axis_angle_to_matrix(const vec1D &, const double);
+vec2D multiply_matrices(const vec2D &, const vec2D &);
+vec2D multiply_matrix_with_vectors(const vec2D &, const vec2D &);
+vec2D get_gonio_rotation_matrix(const vec2D &,
+                                const vec2D &,
+                                const vec1D &,
+                                const double);
+vec2D rotate_goniometer_vectors(const vec2D &,
+                                const vec2D &,
+                                const vec1D &,
+                                const double,
+                                const vec2D &);
