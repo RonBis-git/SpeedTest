b3220213b39a33d2ed19e610b5ca120402b7545c
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-02 12:40:41 +0000

    Made the eigen implementation of goniometer rotation by an angle and applying it on a set of vectors.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from time import perf_counter
+import timeit
 import sys
 
 sys.path.append("build")
@@ -11,19 +12,41 @@ def operations(rows):
 
 
 if __name__ == "__main__":
-    s_mat = np.arange(9).reshape(3, 3)
+    s_mat = np.eye(3).astype(np.float64)
     print(s_mat)
-    f_mat = np.arange(100,109).reshape(3,3)
+    f_mat = np.array(
+    [
+        [0.96502798, 0.05985625, -0.25522191],
+        [-0.12860378, -0.74028049, -0.65988322],
+        [-0.22843393, 0.66962828, -0.70669364],
+    ],
+    dtype=np.float64
+)
     print(f_mat)
-    axis = np.random.default_rng().uniform(0,3,size=3)
-    angle = np.pi/4
-    vecs = operations(rows=2)
+    axis = np.array([1.0,0.0,0.0]).astype(np.float64)
+    angle = np.pi/3
+    vecs = operations(rows=100000)
 
     print('Numpy results')
+    npstart = perf_counter()
     np_rotvecs = npOps.rotate_vectors(s_mat, f_mat, axis, angle, vecs)
-    print(np_rotvecs)
+    npstop = perf_counter()
+    # print(np_rotvecs)
 
     print('Eigen results')
-    eig_rotvecs = cppOps.get_rotated_vectors(s_mat, f_mat, axis, angle, vecs)
-    print(eig_rotvecs)
+    # eig_rotvecs = cppOps.get_rotated_vector(axis)
+    # print(cppOps.print_passed(axis))
+    cppstart = perf_counter()
+    eig_rotvecs = cppOps.eig_get_rotated_vector(s_mat, 
+                                            f_mat, 
+                                            axis/np.linalg.norm(axis), 
+                                            angle, 
+                                            vecs.T).T
+    cppstop = perf_counter()
+    print(np.isclose(eig_rotvecs,np_rotvecs, rtol=1E-7, atol=1E-8))
+    numpy_time = timeit.timeit(lambda: npOps.rotate_vectors(s_mat, f_mat, axis, angle, vecs), number=1000)
 
+    cpp_time = timeit.timeit(lambda: cppOps.eig_get_rotated_vector(s_mat, f_mat, axis, angle, vecs.T), number=1000)
+
+    print(f"NumPy time: {npstop-npstart} secs and {numpy_time} secs")
+    print(f"C++ Eigen time: {cppstop-cppstart} secs and {cpp_time} secs")
