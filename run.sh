9a46b490ca212c028930fe1166f2e9adb4f2e104
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-05 14:35:27 +0000

    Included general statements.

--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-cd -
+# Set the current folder as the target folder 
+target_folder=$(pwd)
+
+# Build directory name
+build_dir="build"
+
+# Check if build directory exists
+if [ ! -d "$target_folder/$build_dir" ]; then
+
+  # Build directory does not exist, create it
+  echo "Creating $build_dir directory."  
+  mkdir "$target_folder/$build_dir"
+else
+  # Build directory already exists
+  echo "$build_dir exists."
+fi
+
+cd $target_folder/$build_dir
+cmake ..
 make
-cd -
+cd ..
 dials.python main.py
