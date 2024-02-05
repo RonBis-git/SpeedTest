be8ce4fb27d32df1365cf4163fc9419ec7d14ab3
RonBis-git <rohanbiswas1993@gmail.com>
2024-02-05 14:03:58 +0000

    Refactor: Divided operations into respective functions. Included plotting of runtimes.
    Several statements have been moved into functions. Names of few variables have been modified. The code now calls get_run_times() with vectors of different sample sizes and records the time taken using perf_counter().

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from time import perf_counter
 import timeit
 import sys
@@ -7,58 +8,108 @@ sys.path.append("build")
 import cppOps
 import npOps
 
-def operations(rows):
-    return np.random.default_rng().uniform(0, 1, size=(rows, 3))
+
+def create_vectors(sample_size):
+    return np.random.default_rng().uniform(0, 1, size=(sample_size, 3))
 
 
-if __name__ == "__main__":
+def create_axis():
+    return np.random.default_rng().uniform(0, 5, size=3)
+
+
+def fixed_and_setting():
+    # Fixed and setting matrix have been hardcoded from an experiment file.
     s_mat = np.eye(3).astype(np.float64)
-    print(s_mat)
     f_mat = np.array(
-    [
-        [0.96502798, 0.05985625, -0.25522191],
-        [-0.12860378, -0.74028049, -0.65988322],
-        [-0.22843393, 0.66962828, -0.70669364],
-    ],
-    dtype=np.float64
-)
-    print(f_mat)
-    axis = np.array([1.0,0.0,0.0]).astype(np.float64)
-    angle = np.pi/3
-    vecs = operations(rows=100000)
+        [
+            [0.96502798, 0.05985625, -0.25522191],
+            [-0.12860378, -0.74028049, -0.65988322],
+            [-0.22843393, 0.66962828, -0.70669364],
+        ],
+        dtype=np.float64,
+    )
+    return s_mat, f_mat
 
-    print('Numpy results')
+
+def get_run_times(s_mat, f_mat, axis, angle, vecs):
+    # print("Numpy results")
     npstart = perf_counter()
     np_rotvecs = npOps.rotate_vectors(s_mat, f_mat, axis, angle, vecs)
     npstop = perf_counter()
     # print(np_rotvecs)
 
-    print('Eigen results')
-    cppstart = perf_counter()
-    eig_rotvecs = cppOps.eig_get_rotated_vector(s_mat, 
-                                            f_mat, 
-                                            axis, 
-                                            angle, 
-                                            vecs.T).T
-    cppstop = perf_counter()
-    print(np.isclose(eig_rotvecs,np_rotvecs, rtol=1E-7, atol=1E-8))
+    # print("Eigen results")
+    cpp_eig_start = perf_counter()
+    eig_rotvecs = cppOps.eig_get_rotated_vector(s_mat, f_mat, axis, angle, vecs.T).T
+    cpp_eig_stop = perf_counter()
+    # print(np.isclose(eig_rotvecs, np_rotvecs, rtol=1e-7, atol=1e-8))
 
-    print('STL results')
-    cppstlstart = perf_counter()
-    stl_rotvecs = cppOps.stl_rotate_goniometer_vectors(s_mat, 
-                                            f_mat, 
-                                            axis, 
-                                            angle, 
-                                            vecs)
-    cppstlstop = perf_counter()
-    print(np.isclose(stl_rotvecs,np_rotvecs, rtol=1E-7, atol=1E-8))
+    # print("STL results")
+    cpp_stl_start = perf_counter()
+    stl_rotvecs = cppOps.stl_rotate_goniometer_vectors(s_mat, f_mat, axis, angle, vecs)
+    cpp_stl_stop = perf_counter()
+    # print(np.isclose(stl_rotvecs, np_rotvecs, rtol=1e-7, atol=1e-8))
 
-    numpy_time = timeit.timeit(lambda: npOps.rotate_vectors(s_mat, f_mat, axis, angle, vecs), number=1000)
+    # numpy_time = timeit.timeit(
+    #     lambda: npOps.rotate_vectors(s_mat, f_mat, axis, angle, vecs), number=1000
+    # )
 
-    cpp_eig_time = timeit.timeit(lambda: cppOps.eig_get_rotated_vector(s_mat, f_mat, axis, angle, vecs.T), number=1000)
-    cpp_stl_time = timeit.timeit(lambda: cppOps.stl_rotate_goniometer_vectors(s_mat, f_mat, axis, angle, vecs), number=1000)
+    # cpp_eig_time = timeit.timeit(
+    #     lambda: cppOps.eig_get_rotated_vector(s_mat, f_mat, axis, angle, vecs.T),
+    #     number=1000,
+    # )
+    # cpp_stl_time = timeit.timeit(
+    #     lambda: cppOps.stl_rotate_goniometer_vectors(s_mat, f_mat, axis, angle, vecs),
+    #     number=1000,
+    # )
+
+    # print(f"NumPy time: {npstop-npstart} secs and {numpy_time} secs")
+    # print(f"C++ Eigen time: {cppstop-cppstart} secs and {cpp_eig_time} secs")
+    # print(f"C++ STL time: {cppstlstop-cppstlstart} secs and {cpp_stl_time} secs")
+
+    return npstop - npstart, cpp_eig_stop - cpp_eig_start, cpp_stl_stop - cpp_stl_start
+
+def plot_run_times(sample_size, np_times, eig_times, stl_times):
+    import matplotlib.pyplot as plt
+    
+    plt.plot(sample_size,np_times,label='numpy')
+    plt.plot(sample_size,eig_times,label='Eigen')
+    plt.plot(sample_size,stl_times,label='std::vector')
+
+    plt.xlabel('Vector Sample Size')
+    plt.ylabel('Run time (secs)')
+
+    plt.legend(loc='best')
+    plt.xscale('log')
+    plt.yscale('log')
+
+    plt.show()
 
 
-    print(f"NumPy time: {npstop-npstart} secs and {numpy_time} secs")
-    print(f"C++ Eigen time: {cppstop-cppstart} secs and {cpp_eig_time} secs")
-    print(f"C++ STL time: {cppstlstop-cppstlstart} secs and {cpp_stl_time} secs")
+if __name__ == "__main__":
+
+    # Input params
+    angle = np.pi / 3
+    s_mat, f_mat = fixed_and_setting()
+    axis = create_axis()
+
+    # Initialising sample_size and output timing arrays
+
+    sample_size = np.array([10, 100, 1000, 10_000, 100_000, 1_000_000])
+    np_times = []
+    eig_times = []
+    stl_times = []
+
+    # Running the timing function for each sample size
+
+    for i in range(len(sample_size)):
+        np_t, eig_t, stl_t = get_run_times(
+            s_mat, f_mat, axis, angle, create_vectors(sample_size[i])
+        )
+        np_times.append(np_t)
+        eig_times.append(eig_t)
+        stl_times.append(stl_t)
+
+    print(np_times)
+    #Plotting the run times 
+    plot_run_times(sample_size, np_times, eig_times, stl_times)
